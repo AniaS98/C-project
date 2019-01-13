85475b7423b9d4e87ea22a3ed61424fba21a6537
Justyna Sobczyk <justynasobczyk6@gmail.com>
2019-01-13 20:05:53 +0100

    nowe gui

--- gui/gui/WindowKlientAlergeny.xaml.cs
+++ gui/gui/WindowKlientAlergeny.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -18,9 +19,16 @@ namespace gui
     /// </summary>
     public partial class WindowKlientAlergeny : Window
     {
+        Dieta dieta = new Dieta();
+        ObservableCollection<SAlergen> lista;
+
         public WindowKlientAlergeny()
         {
             InitializeComponent();
+            lista = new ObservableCollection<SAlergen>();
+            dieta = (Dieta)Dieta.OdczytajXML("dieta2.xml"); //tu właściwa ścieżka!
+            lista = new ObservableCollection<SAlergen>(dieta.ERodzajAlergenu);
+
         }
     }
 }
