f64dcd2058301778fa0cf8c8f0d2c6312d82e863
Michaela Wojdan <misia.wojdan@wp.pl>
2019-01-18 23:11:41 +0100

    sprawdzenie numeru PESEL

--- ProjektPO/ProjektPO/Osoba.cs
+++ ProjektPO/ProjektPO/Osoba.cs
@@ -22,7 +22,53 @@ namespace Projekt
 
         public string Imie { get => imie; set => imie = value; }
         public string Nazwisko { get => nazwisko; set => nazwisko = value; }
-        public string Pesel { get => pesel; set => pesel = value; }
+        public string Pesel
+        {
+            get
+            {
+                return pesel;
+            }
+
+            set
+            {
+                int kontrolna;
+
+                if (value.Length != 11)
+                {
+                    Console.WriteLine("Niepoprawna długość numeru PESEL!");
+                    throw new WronPESELExcepition();
+                }
+
+                if ((value[9] - '0')%2==0)
+                {
+                    if (plec != Plcie.K)
+                    {
+                    Console.WriteLine("Niezgodność podanej płci z numerem Pesel!");
+                    throw new WronPESELExcepition();
+                    }
+                }
+
+                if ((value[9] - '0') % 2 != 0)
+                {
+                    if (plec != Plcie.M)
+                    {
+                    Console.WriteLine("Niezgodność podanej płci z numerem Pesel!");
+                    throw new WronPESELExcepition();
+                    }
+                }
+
+                kontrolna= 9*(value[0] - '0') + 7*(value[1] - '0') + 3*(value[2] - '0') + (value[3] - '0') + 9*(value[4] - '0') + 7*(value[5] - '0') + 3*(value[6] - '0') + (value[7] - '0') + 9*(value[0] - '8') + 7*(value[9] - '0');
+                kontrolna %= 10;
+
+                if(kontrolna != (value[10] - '0'))
+                {
+                Console.WriteLine("Błąd w numerze Pesel - niepoprawna liczba kontrolna!");
+                throw new WronPESELExcepition();
+                }
+                       
+                pesel = value;
+            }
+        }
         internal Plcie Plec { get => plec; set => plec = value; }
 
         public Osoba()
