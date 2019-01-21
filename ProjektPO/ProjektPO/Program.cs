bc67ed1be82c260540ae5770b9d5b07a9faca768
Tala <tala.roz9@gmail.com>
2019-01-21 20:30:39 +0100

    interfejs

--- ProjektPO/ProjektPO/Program.cs
+++ ProjektPO/ProjektPO/Program.cs
@@ -8,9 +8,44 @@ namespace Projekt
 {
     class Program
     {
+     
         static void Main(string[] args)
-        { 
+        {
+            int wybor;
+            Console.WriteLine("Firma cateringowa!");
+            Console.WriteLine("Wybierz opcje 1-klient, 2-pracownik lub 0-wyjscie: \n");
+            wybor = Console.Read();
+            switch (wybor)
+            {
+                case '0':
+                    {
+                        break;
+                    }
+                case '1':
+                    {
+                        
+                        break;
+                    }
+                case '2':
+                    {
+                        string login;
+                        string haslo;
+                        Console.WriteLine("Login:");
+                        login = Console.ReadLine();
+                        Console.WriteLine("Hasło: ");
+                        haslo = Console.ReadLine();
+                        while(haslo!="admin"&& login!="admin")
+                        {
+                            Console.WriteLine("Zły login lub hasło!");
+                        }
+
+                        Console.WriteLine("!!!");
+                        break;
+                    }
+            }
+
             Console.ReadKey();
-        }
+        } 
+        
     }
 }
