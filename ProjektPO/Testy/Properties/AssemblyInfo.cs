2bad8ed97f785bd6b0b5ed7d520adc297166e80d
AniaS98 <44135167+AniaS98@users.noreply.github.com>
2019-01-22 01:09:02 +0100

    Testy jednostkowe

---
+++ ProjektPO/Testy/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testy")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("919fde9a-7a6b-44a3-ace8-5c9315089556")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProjektPO/Testy/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projekt;
+
+namespace Testy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string nazwa = "zespoltestowy1";
+            Zespol z1 = new Zespol("zespoltestowy1");
+            Assert.AreEqual(nazwa, z1.Nazwa);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Pracownik c1 = new Pracownik("Jan", "Nowak","11111111111",Plcie.M);
+            Pracownik c2 = new Pracownik("Adam", "Nowak","22222222222",Plcie.M);
+            Assert.AreEqual(1, c1.CompareTo(c2));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Adres a1 = new Adres("Poludniowa", "28A", 44, "31-241", "Krakow");
+            //Ja bym tu dala klonowanie, ale nie wiem :(
+        }
+
+    }
+}
